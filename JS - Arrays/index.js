2832bf145008779d11449230e3fe7e2b69cf099c
Kənan Ələkbərov <kananha@code.edu.az>
2023-05-04 04:04:23 +0400

    arrays task

---
+++ JS - Arrays/index.js
@@ -0,0 +1,41 @@
+// // let number = [1, 2, 3, 4, 1, 5, 3];
+// // let set = new Set(number);
+
+// let duplicates = number.filter(item => {
+
+//     if (set.has(item)) {
+//         set.delete(item);
+//     }
+//     else {
+//         return item;
+//     }
+// });
+// console.log(duplicates);
+
+
+
+// // let array = [1, 5, 4, 7, 9, 2, 3, 15, 10,];
+// // array.sort((a, b) => a - b);
+// // console.log(array);
+
+// // function largeNum(arr) {
+// //     for (i = 0; i < array.length; i++) {
+// //         return arr[array.length - 1];
+// //     }
+// // }
+// // console.log(largeNum(array));
+
+
+
+// // let array = [1, 2, 3, 4, 5,];
+
+// // function arrayHandler(item) {
+// //     return item * 2;
+// // }
+// // let newArr = array.map(arrayHandler);
+// // console.log(newArr);
+
+
+// // let numbers = [10, 20, 30, 40];
+// // let newArray = numbers.map(a => a * 2);
+// // console.log(newArray);
